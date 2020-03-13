62a83f2dac8db0366e6fde4cfc8ec6ea10019b87
hduWendy <576939544@qq.com>
2020-03-13 14:38:54 +0800

    。

--- 11_iRange_Extract_target_pixel/11_iRange_Extract_target_pixel.cpp
+++ 11_iRange_Extract_target_pixel/11_iRange_Extract_target_pixel.cpp
@@ -2,10 +2,45 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
-}
+	VideoCapture cap(0);
 
+	double scale = 0.5;
+
+	double i_minH = 0;
+	double i_maxH = 20;
+
+	double i_minS = 43;
+	double i_maxS = 255;
+
+	double i_minV = 55;
+	double i_maxV = 255;
+
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat hsvMat;
+		cv::Mat detectMat;
+
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+		rFrame.copyTo(detectMat);
+
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+
+		cv::imshow("whie: in the range", detectMat);
+		cv::imshow("frame", rFrame);
+
+		waitKey(30);
+	}
+}
